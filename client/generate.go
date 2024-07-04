aa9bbbe665e481f968baf89c7dafb2b4e6473014
franklin <kevin@franklinkim.de>
2024-07-04 13:10:51 +0200

    chore: enable linters

--- client/generate.go
+++ client/generate.go
@@ -1,4 +1,4 @@
 package client
 
-////go:generate curl -f -H "Accept: application/vnd.github.v3.raw" -o "client.yaml" -L "https://raw.githubusercontent.com/teamhanko/hanko/backend/v0.11.0/docs/static/spec/public.yaml"
+// generate curl -f -H "Accept: application/vnd.github.v3.raw" -o "client.yaml" -L "https://raw.githubusercontent.com/teamhanko/hanko/backend/v0.11.0/docs/static/spec/public.yaml"
 //go:generate oapi-codegen -generate types,client -config client.config.yaml -o client_gen.go client.yaml
